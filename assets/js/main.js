7048c530b55581927be404e616fb46847a1b8a55
My-V0ice <108202587+My-V0ice@users.noreply.github.com>
2023-10-04 14:50:49 +1000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,19 @@
+// open
 const bars = document.querySelector(".menu__btn");
+
+// close
+const onBars = document.querySelector(".menu__btn2");
 
 // Открытие меню
 bars.addEventListener('click', function (event) {
     document.querySelector(".nav__list").style.display = "block";
+
+    // Изменение иконки
+    onBars.style.display = "block";
+    bars.style.display = "none";
 });
 
+// Закрытие меню
 // Поиск всех элементов с классом ".nav__item"
 const items = document.querySelectorAll('.nav__item')
 
@@ -12,5 +21,17 @@ const items = document.querySelectorAll('.nav__item')
 items.forEach(element => {
     element.addEventListener('click', function (event) {
         document.querySelector(".nav__list").style.display = "none";
+
+        // Изменение иконки
+        onBars.style.display = "none";
+        bars.style.display = "block";
     })
+})
+
+onBars.addEventListener("click", function (event) {
+    document.querySelector(".nav__list").style.display = "none";
+
+    // Изменение иконки
+    onBars.style.display = "none";
+    bars.style.display = "block";
 })
